97971881d387792dcc12cae24f4a6bb971d80151
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-10-23 13:27:56 -0500

    can create a user, store info as cookies, login with user stored in mongoDB

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.post("/signup", async (req, res) => {
     await client.connect();
     const database = client.db("Spark");
     const users = database.collection("users");
-
     const existingUser = await users.findOne({ email });
+
     if (existingUser) {
       return res.status(409).send("User already exists, please login");
     }
@@ -49,9 +49,37 @@ app.post("/signup", async (req, res) => {
     const token = jwt.sign(insertedUser, sanitizedEmail, {
       expiresIn: 60 * 24,
     });
-    res
-      .status(201)
-      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+    res.status(201).json({ token, userId: generatedUserId });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const client = new MongoClient(uri);
+  const { email, password } = req.body;
+
+  // define database
+  try {
+    await client.connect();
+    const database = client.db("Spark");
+    const users = database.collection("users");
+    // find user in database by email
+    const user = await users.findOne({ email });
+
+    const correctPassword = await bcrypt.compare(
+      password,
+      user.hashed_password
+    );
+
+    // check password entered with hashed password stored in DB
+    if (user && correctPassword) {
+      const token = jwt.sign(user, email, {
+        expiresIn: 60 * 24,
+      });
+      res.status(201).json({ token, userId: user.user_id });
+    }
+    res.status(400).send("Invalid Credentials");
   } catch (err) {
     console.log(err);
   }
@@ -68,6 +96,39 @@ app.get("/users", async (req, res) => {
     res.send(returnedUsers);
   } finally {
     // client will close when finished, or if there is an error
+    await client.close();
+  }
+});
+
+// put for updating a document
+app.put("/user", async (req, res) => {
+  const client = new MongoClient(uri);
+  const formData = req.body.formData;
+
+  try {
+    await client.connect();
+    const database = client.db("Spark");
+    const users = database.collection("users");
+
+    const query = { user_id: formData.user_id };
+    const updateDocument = {
+      $set: {
+        first_name: formData.first_name,
+        dob_day: formData.dob_day,
+        dob_month: formData.dob_month,
+        dob_year: formData.dob_year,
+        show_gender: formData.show_gender,
+        gender_identity: formData.gender_identity,
+        gender_interest: formData.gender_interest,
+        url: formData.url,
+        about: formData.about,
+        matches: formData.matches,
+      },
+    };
+    // update the user 'query' with 'updateDocument' values.
+    const insertedUser = await users.updateOne(query, updateDocument);
+    res.send(insertedUser);
+  } finally {
     await client.close();
   }
 });
